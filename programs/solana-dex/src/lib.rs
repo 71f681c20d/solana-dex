ab21d4a27605ce9db1ec41d18ae4aaebfc1834fa
Alexander Barnard <c9ed0316fa@protonmail.com>
2024-03-20 19:01:11 +0700

    Created initialize mint accounts

--- programs/solana-dex/src/lib.rs
+++ programs/solana-dex/src/lib.rs
@@ -1,5 +1,6 @@
 use anchor_lang::prelude::*;
 use spl_token_swap::state::SwapVersion;
+use anchor_spl::token::{Mint, Token };
 
 declare_id!("GBgHzis7UarrQYHo9viMjhzVEAcVvo7KLPbyShwdWA2M");
 
@@ -27,6 +28,31 @@ pub struct InitializeTokenSwapAccount<'info> {
     token_swap_account: AccountInfo<'info>, 
     /// CHECK: This is safe since we don't read or write from this account
     token_swap_program: AccountInfo<'info>, 
+    system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct InitializeMintAccounts<'info> {
+    #[account(mut)]
+    signer: Signer<'info>, 
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    mint_authority: AccountInfo<'info>, 
+    #[account(
+        init, 
+        payer=signer, 
+        mint::decimals = 9, 
+        mint::authority = mint_authority
+    )]
+    x_mint: Account<'info, Mint>,
+    #[account(
+        init, 
+        payer=signer, 
+        mint::decimals = 9, 
+        mint::authority = mint_authority
+    )]
+    y_mint: Account<'info, Mint>,
+    token_program: Program<'info, Token>,
+    rent: Sysvar<'info, Rent>,
     system_program: Program<'info, System>,
 }
 
