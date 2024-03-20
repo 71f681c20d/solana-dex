dbb4b4fb88c6cf0d2b39242f3cdc7e65f0ef7b0d
Alexander Barnard <c9ed0316fa@protonmail.com>
2024-03-20 18:25:00 +0700

    Added TokenSwapAccount initialization

--- programs/solana-dex/src/lib.rs
+++ programs/solana-dex/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use spl_token_swap::state::SwapVersion;
 
 declare_id!("GBgHzis7UarrQYHo9viMjhzVEAcVvo7KLPbyShwdWA2M");
 
@@ -6,10 +7,26 @@ declare_id!("GBgHzis7UarrQYHo9viMjhzVEAcVvo7KLPbyShwdWA2M");
 pub mod solana_dex {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize_token_swap_account(ctx: Context<InitializeTokenSwapAccount>) -> Result<()> {
+        msg!("Created Token Swap State Account---");
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct InitializeTokenSwapAccount<'info> {
+    #[account(mut)]
+    signer: Signer<'info>, 
+    #[account(
+        init, 
+        payer=signer, 
+        space=SwapVersion::LATEST_LEN, 
+        owner=token_swap_program.key()
+    )]
+    /// CHECK: This is safe since we don't read or write from this account
+    token_swap_account: AccountInfo<'info>, 
+    /// CHECK: This is safe since we don't read or write from this account
+    token_swap_program: AccountInfo<'info>, 
+    system_program: Program<'info, System>,
+}
+
